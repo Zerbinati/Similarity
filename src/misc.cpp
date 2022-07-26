9ad66e94bca72dfcaa494c8897e553b29db1d30d
Zerbinati <zerbinati.marco@gmail.com>
2022-07-26 19:36:22 +0200

    t1

--- src/misc.cpp
+++ src/misc.cpp
@@ -250,7 +250,7 @@ string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "HypnoS exp1_t1" << Version << setfill('0');
+  ss << "HypnoS exp1_t1 " << Version << setfill('0');
 
   if (Version.empty())
   {
